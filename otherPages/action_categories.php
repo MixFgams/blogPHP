1b7c23200fbd854a439fd4bf1bf535550ff86a32
MikoThan <129270315+MikoThan@users.noreply.github.com>
2024-10-22 11:43:34 +0200

    Add files via upload

--- otherPages/action_categories.php
+++ otherPages/action_categories.php
@@ -1,14 +1,18 @@
-<?
-require_once('header.php');
+<?php
+require_once('connexion.php');
 
 function valid_modif(){
-    return isset($_POST['valider']);
+    return (isset($_POST['valider']) && ($_SERVER['REQUEST_METHOD'] === 'POST'));
+}
+
+function verif_admin(){
+    return ($_SESSION['pw']==="admin69IUT" && $_SESSION['email']==="admin@localhost.fr");
 }
 
 
-if ($_SESSION['connected'] && $_SESSION['connect']->$DBusername==="root"){
+if ($_SESSION['connected'] && verif_admin()){
     if (isset($_POST['create'])){
-        echo "<form>
+        echo "<form method='POST'>
                 <label>Entrez le nom de la catégorie à créer</label>
                 <input type='text' name='newcat' id='newcat' placeholder='nom catégorie à créer' required>
             </form>";
@@ -18,7 +22,7 @@ if ($_SESSION['connected'] && $_SESSION['connect']->$DBusername==="root"){
         }
     }
     else if (isset($_POST['update'])){
-        echo "<form>
+        echo "<form method='POST'>
                 <label>Entrez le nom de la catégorie à modifier</label>
                 <input type='text' name='catmodif' id='catmodif' placeholder='nom catégorie à modifier' required>
                 <label>Entrez le nouveau nom</label>
@@ -33,17 +37,24 @@ if ($_SESSION['connected'] && $_SESSION['connect']->$DBusername==="root"){
 
     }
     else if(isset($_POST['read'])){
-        echo "<form>
+        echo "<form method='POST'>
                 <label>Entrez le nom de la catégorie à afficher</label>
                 <input type='text' name='cataffich' id='cataffich' placeholder='nom catégorie à afficher' required>
             </form>";
             if (!empty($_POST['cataffich']) && valid_modif()){
                 $requete='select * from Categorie where nom=?';
                 exec_request($requete, 's', [$_POST['cataffich']]);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) { // pour chaque ligne renvoyée
+                        echo "Catégorie: " . htmlspecialchars($row['nom']) . "<br>"; // affiche le nom de la catégorie. htmlspecialchars rajoute une sécurité dans son affichage du nom
+                    }
+                } else {
+                    echo "Aucune catégorie trouvée.";
+                }
             }
     }
     else if (isset($_POST['delete'])){
-        echo "<form>
+        echo "<form method='POST'>
                 <label>Entrez le nom de la catégorie à supprimer</label>
                 <input type='text' name='catdelete' id='catdelete' placeholder='nom catégorie à supprimer' required>
             </form>";
@@ -52,6 +63,14 @@ if ($_SESSION['connected'] && $_SESSION['connect']->$DBusername==="root"){
                 exec_request($requete, 's', [$_POST['catdelete']]);
             }
     }
+}
+elseif ($_SESSION['connect']->$DBusername!=="root"){
+    header('Location: ../index.php');
+    exit();
+}
+else{
+    header('Location: connexion.php');
+    exit();
 }
 
 
--- otherPages/article.php
+++ otherPages/article.php
@@ -11,15 +11,28 @@ class Article
     public $comm = [];
     
     public function displayArticle(){
-        echo "<img src= $this->img/>";
+        //echo "<img src= $this->img/>";
         echo "$this->titre";
         echo "$this->desc";
         echo "$this->pseudo";
-
-
+        for($i=0;$i<count($this->comm);$i++){
+        echo"$this->comm";}
     }
 
 }
 
 
 ?>
+
+<html>
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="img/obLogo.png" type="image/x-icon">
+        <link rel="stylesheet" href="otherPages/connexion.css"> 
+        <title>Connexion</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
