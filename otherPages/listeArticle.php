bf8b065df84cf045a224b6641d9c5cc2615cc977
Marwane-EL <marw2ne4007@gmail.com>
2024-11-01 20:43:19 +0100

    Update listeArticle.php

--- otherPages/listeArticle.php
+++ otherPages/listeArticle.php
@@ -15,7 +15,6 @@
         die("Échec de la connexion : " . $conn->connect_error);
     }
 
-
 ?>
 
 <html>
@@ -24,15 +23,24 @@
         <?php
             include 'header.php';
         ?>
-        <h1>Liste des articles</h1>
     </header>
     <body>
-    <?php
-        $sql = "SELECT id, titre, description, fk_pseudo FROM article";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<section class='article'>
+    <main>
+    <div id="search-container">
+
+        <form method="GET">
+            <label for="barreRecherche">Recherchez un article : </label>
+            <input id="barreRecherche" name="barreRecherche" type="search">
+            <?php
+            $_isSearched = false;
+            if(!empty($_GET['barreRecherche'])){
+                $_isSearched = true;
+                $sql = "SELECT id, titre, description, fk_pseudo FROM article WHERE fk_pseudo LIKE '%".$_GET['barreRecherche']."%'";
+                $result = $conn->query($sql);
+                echo "<h2>Articles trouvés avec le pseudo : " . htmlspecialchars($_GET['barreRecherche']) . "</h2>";
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<section class='article'>
                         <h2>" . $row["titre"] . "</h2>
                         <p>" . $row["description"] . "</p>
                         <div class='article-footer'>
@@ -40,11 +48,42 @@
                         </div>
                         <a href=''><p class='lienArticle'>Afficher plus</p></a>
                       </section>";
+                    }
+                } else {
+                    echo "<p>Aucun article trouvé avec le pseudo : " . htmlspecialchars($_GET['barreRecherche']) . "</p>";
+                }
             }
-        } else {
-            echo "Aucun article trouvé.";
+            ?>
+        </form>
+
+    </div>
+    <h1>Liste des articles</h1>
+    <?php
+        if($_isSearched == false){
+            $sql = "SELECT id, titre, description, fk_pseudo FROM article";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<section class='article'>
+                        <h2>" . $row["titre"] . "</h2>
+                        <p>" . $row["description"] . "</p>
+                        <div class='article-footer'>
+                            <span class='auteur'>Auteur : " . $row["fk_pseudo"] . "</span>
+                        </div>
+                        <a href=''><p class='lienArticle'>Afficher plus</p></a>
+                      </section>";
+                }
+            } else {
+                echo "Aucun article trouvé.";
+            }
         }
+
     ?>
+
+    <?php
+
+    ?>
+    </main>
     </body>
     <?php
         include 'footer.php'
